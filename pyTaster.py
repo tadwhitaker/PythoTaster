c1733e42724ad4de8db4bae1342ff5d5e239fa4d
Tad <tad@whitakerresearch.com>
2015-12-03 12:29:27 -0800

    Simple function call, random number and conditional statement.

---
+++ pyTaster.py
@@ -0,0 +1,16 @@
+from random import randint
+
+list = ['Tad', 'Terry', 'Fountain']
+
+for i in list: print 'Name: ', i
+
+print (randint(0, 100))
+
+bike = "Haro"
+
+def tester(z):
+    print z
+    return
+
+tester(bike)
+
